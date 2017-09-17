9751e9b3eb3e7d2909534e698428ce928507ad89
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2017-09-18 00:08:55 +0900

    test code update

--- python/test.py
+++ python/test.py
@@ -24,16 +24,15 @@ class MyParser(object):
                                   )
         self.args = self._parser.parse_args(namespace=self)
 
-if __name__ == '__main__':
-    parser = MyParser()
 
+def main(parser):
     port = parser.args.port
     baud = parser.args.baud
     dev = serial2i2c(port, baud)
 
-    raw_input("I2C: use channel 0")
-    print Fore.CYAN + Style.BRIGHT + "\tdev.setChannel(0)" + Style.RESET_ALL +\
-        " -> " + dev.setChannel(0)
+    input("I2C: use channel 0")
+    print(Fore.CYAN + Style.BRIGHT + "\tdev.setChannel(0)" + Style.RESET_ALL +
+          " -> " + dev.setChannel(0))
 
     s = """
     I2C: on each channel:
@@ -41,61 +40,66 @@ if __name__ == '__main__':
     then get and list ACK/NAK
     """
     if False:
-        raw_input(s)
+        input(s)
         for ch in range(4):
-            print ch
+            print (ch)
             dev.setChannel(ch)
-            print "--, 00, 02, 04, 06, 08, 0A, 0C, 0E"
+            print("--, 00, 02, 04, 06, 08, 0A, 0C, 0E")
             for msb in range(0x00, 0x100, 0x10):
-                print "%02X," % (msb),
+                print("%02X," % (msb), end="")
                 for lsb in range(0, 0x10, 0x02):
                     if (dev.write(msb | lsb, 0x00).split(",")[0] == "ACK"):
-                        print ">A<",
+                        print(">A<", end=" ")
                     else:
-                        print " N ",
-                print ""
+                        print(" N ", end=" ")
+                print ("")
 
-    raw_input("Registers: read registers 0/1/2/3/4 by API")
-    print Fore.CYAN + Style.BRIGHT + "\tdev.reg_read('01234')" + Style.RESET_ALL +\
-        " -> " + dev.reg_read('01234')
+    input("Registers: read registers 0/1/2/3/4 by API")
+    print (Fore.CYAN + Style.BRIGHT + "\tdev.reg_read('01234')" + Style.RESET_ALL +
+           " -> " + dev.reg_read('01234'))
 
-    raw_input("Registers: write single data `0x55` to registers 1/3 by API")
-    print Fore.CYAN + Style.BRIGHT +\
-        "\tdev.reg_write([[dev.GPIO0_STAT, 0x55], [dev.GPIO0_CONF, 0x55]])" +\
-        Style.RESET_ALL +\
-        " -> " + dev.reg_write([[dev.GPIO0_STAT, 0x55], [dev.GPIO0_CONF, 0x55]])
+    input("Registers: write single data `0x55` to registers 1/3 by API")
+    print (Fore.CYAN + Style.BRIGHT +
+           "\tdev.reg_write([[dev.GPIO0_STAT, 0x55], [dev.GPIO0_CONF, 0x55]])" +
+           Style.RESET_ALL +
+           " -> " + dev.reg_write([[dev.GPIO0_STAT, 0x55], [dev.GPIO0_CONF, 0x55]]))
 
-    raw_input("GPIO status: reads registers 0/1/2/3/4 " +
-              "(where 0/3/4 returns 0xAA) by 'I' command using low level API")
+    input("GPIO status: reads registers 0/1/2/3/4 " +
+          "(where 0/3/4 returns 0xAA) by 'I' command using low level API")
     dev.raw_write('I01234P')
-    print Fore.CYAN + Style.BRIGHT + "\tdev.raw_write('I01234P')" +\
-        Style.RESET_ALL + " -> " + dev.raw_read()
+    print (Fore.CYAN + Style.BRIGHT + "\tdev.raw_write('I01234P')" +
+           Style.RESET_ALL + " -> " + dev.raw_read())
 
-    raw_input("Registers: reads registers 0/1/2/3/4 again")
-    print Fore.CYAN + Style.BRIGHT + "\tdev.reg_read('01234')" + Style.RESET_ALL +\
-        " -> " + dev.reg_read('01234')
+    input("Registers: reads registers 0/1/2/3/4 again")
+    print (Fore.CYAN + Style.BRIGHT + "\tdev.reg_read('01234')" + Style.RESET_ALL +
+           " -> " + dev.reg_read('01234'))
 
-    raw_input("Registers: reads register 'GPIO0_CONF' by low level API")
+    input("Registers: reads register 'GPIO0_CONF' by low level API")
     dev.raw_write('R' + dev.GPIO0_CONF + 'P')
-    print Fore.CYAN + Style.BRIGHT + \
-        "\tdev.raw_write('R' + dev.GPIO0_CONF + 'P'); dev.raw_read()" +\
-        Style.RESET_ALL + " -> " + dev.raw_read()
+    print (Fore.CYAN + Style.BRIGHT +
+           "\tdev.raw_write('R' + dev.GPIO0_CONF + 'P'); dev.raw_read()" +
+           Style.RESET_ALL + " -> " + dev.raw_read())
 
-    raw_input("Registers: writes single data '0x08' to register 'SPI_CONF' by" +
-              " low level API")
-    print Fore.CYAN + Style.BRIGHT + "\tdev.reg_write([(dev.SPI_CONF, 0x08)])" +\
-        Style.RESET_ALL + " -> " + dev.reg_write([(dev.SPI_CONF, 0x08)])
+    input("Registers: writes single data '0x08' to register 'SPI_CONF' by" +
+          " low level API")
+    print (Fore.CYAN + Style.BRIGHT + "\tdev.reg_write([(dev.SPI_CONF, 0x08)])" +
+           Style.RESET_ALL + " -> " + dev.reg_write([(dev.SPI_CONF, 0x08)]))
 
-    raw_input("Registers: writes single data '0x80' to register 'GPIO0_CONF' by" +
-              " low level API")
-    print Fore.CYAN + Style.BRIGHT + "\tdev.reg_write([[dev.GPIO0_CONF, 0x80]])" +\
-        Style.RESET_ALL + " -> " + dev.reg_write([(dev.GPIO0_CONF, 0x80)])
+    input("Registers: writes single data '0x80' to register 'GPIO0_CONF' by" +
+          " low level API")
+    print (Fore.CYAN + Style.BRIGHT + "\tdev.reg_write([[dev.GPIO0_CONF, 0x80]])" +
+           Style.RESET_ALL + " -> " + dev.reg_write([(dev.GPIO0_CONF, 0x80)]))
 
-    raw_input("Registers: writes single data '0x80' to register 'GPIO0_STAT' by" +
-              " low level API")
-    print Fore.CYAN + Style.BRIGHT + "\tdev.reg_write([(dev.GPIO0_STAT, 0x80)])" +\
-        Style.RESET_ALL + " -> " + dev.reg_write([(dev.GPIO0_STAT, 0x80)])
+    input("Registers: writes single data '0x80' to register 'GPIO0_STAT' by" +
+          " low level API")
+    print (Fore.CYAN + Style.BRIGHT + "\tdev.reg_write([(dev.GPIO0_STAT, 0x80)])" +
+           Style.RESET_ALL + " -> " + dev.reg_write([(dev.GPIO0_STAT, 0x80)]))
 
-    raw_input("SPI: writes 2byte data '0x6000' to SPI device by API")
-    print Fore.CYAN + Style.BRIGHT + "\tdev.write_and_read_SPI(2, 0, 0x6000)" +\
-        Style.RESET_ALL + " -> " + dev.write_and_read_SPI(2, 0, 0x6000)
+    input("SPI: writes 2byte data '0x6000' to SPI device by API")
+    print (Fore.CYAN + Style.BRIGHT + "\tdev.write_and_read_SPI(2, 0, 0x6000)" +
+           Style.RESET_ALL + " -> " + dev.write_and_read_SPI(2, 0, 0x6000))
+
+
+if __name__ == '__main__':
+    parser = MyParser()
+    main(parser)
